c794e070c64e5c172f44c7afda8d5c9d337c4f83
Mohamed Eldesoky <max9700man@gmail.com>
2026-08-20 03:44:03 +0300

    Delete urlfilter.sh

--- urlfilter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-input="urls.txt"
-grep -iE "\.js(\?.*)?$" "$input" > js.txt
-grep -E "\?" "$input" > parameters.txt
-grep -viE "\.(js|css|jpg|jpeg|png|gif|svg|ico|webp|bmp|tiff|woff|woff2|ttf|eot|json)(\?.*)?$" "$input" > cleaned.txt
